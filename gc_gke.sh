87712a066243d9d4ec15695a451292b406a1c2df
Gerd Aschemann <gerd@aschemann.net>
2019-01-05 21:10:05 +0100

    Draft create

---
+++ gc_gke.sh
@@ -0,0 +1,19 @@
+
+
+###################################################################################################
+#
+#  WARNING: this command is experimental and the generated script should be executed at the users own risk.  We use this
+#  generated command on the Jenkins X project itself but it has not been tested on other clusters.
+#
+###################################################################################################
+
+# No firewalls found for deletion
+
+gcloud compute disks delete --zone=us-east1-b --quiet gke-jx-gke-181223-1315-pvc-80e6349d-06ae-11e9-a9cd-42010a8e0038
+ gcloud compute disks delete --zone=us-east1-b --quiet gke-jx-gke-181223-1315-pvc-80e723d8-06ae-11e9-a9cd-42010a8e0038
+ gcloud compute disks delete --zone=us-east1-b --quiet gke-jx-gke-181223-1315-pvc-80e849be-06ae-11e9-a9cd-42010a8e0038
+ gcloud compute disks delete --zone=us-east1-b --quiet gke-jx-gke-181223-1315-pvc-80e94a70-06ae-11e9-a9cd-42010a8e0038
+ gcloud compute disks delete --zone=us-east1-b --quiet gke-jx-gke-181223-1315-pvc-80eb5c8f-06ae-11e9-a9cd-42010a8e0038
+ gcloud compute disks delete --zone=us-east1-b --quiet gke-jx-gke-181223-1315-pvc-a379d60c-06be-11e9-ab1a-42010a8e008d
+
+
